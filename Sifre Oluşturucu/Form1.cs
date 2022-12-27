4794dafe72ee4915025b9d501d3ce68db7672c39
mertcangoksu <117410814+Mertcangoksu@users.noreply.github.com>
2022-12-27 20:09:21 +0300

    12 Haneli Sifre Olusturucu
    Büyük harf ve küçük harf bulunduran 12 haneli random şifre oluşturucu .

---
+++ Sifre Oluşturucu/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sifre_Oluşturucu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            string karakterler = "QWERTYUIOPĞÜASDFGHJKLŞİZXCVBNMÖÇqwertyuıopğüasdfghjklşizxcvbnmöç1234567890";
+            string sonuc = "";
+            for (int i = 0; i <= 12; i++)
+            {
+                sonuc += karakterler[rastgele.Next(karakterler.Length)];
+
+            }
+            label1.Text = sonuc;
+        }
+    }
+}
---
+++ Sifre Oluşturucu/Password.cs/Password.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sifre_Oluşturucu.Password.cs
+{
+    internal class Password
+    {
+    }
+}
